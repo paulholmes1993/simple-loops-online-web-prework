ce2d4f97f3064d90ec5125147983934185c0d950
Paul Holmes <paulholmes1993@gmail.com>
2019-05-12 22:15:44 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,8 @@ def loop_iterator(number_of_times)
     counter <= number_of_times
     puts "Welcome to Flatiron School's Web Development Course!"
     counter += 1
-    
+    if counter >= number_of_times
+      break
 end
 
 def times_iterator(number_of_times)
